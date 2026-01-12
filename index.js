93906ef0255eaeab8d25363081b0c785c2c99872
david musembi <dmusembi702@gmail.com>
2026-01-13 01:57:57 +0300

    added codes on index.js

--- index.js
+++ index.js
@@ -4,3 +4,20 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+// creating funtion and assigning argument
+function combineUsers(...args) {
+  
+  const combinedObject = {
+    users:[],
+    merge_date: Date.today().toString("M/d/yyyy")
+  }
+
+  // looping through arg
+  args.forEach(userArray => {
+
+    // merging arrays
+  combinedObject.users.push(...userArray)  
+  }); 
+// Returning the object
+  return combinedObject;
+}
